813a42982c863f71cdbf7d2e9e83bb3185edd3bc
neochentwhia <neochentwhia@gmail.com>
2020-04-04 01:45:02 +0800

    modify server code in "PatientIndexPage.aspx.cs"

--- Code/DBProject/Patient/PatientIndexPage.aspx.cs
+++ Code/DBProject/Patient/PatientIndexPage.aspx.cs
@@ -16,63 +16,6 @@ namespace DBProject.Patient
             sBirthDate.Attributes.Add("onfocus", "(this.type='date')");
         }
 
-        //-----------------------Function1--------------------------//
-        protected void loginV(object sender, EventArgs e)
-        {
-            string email = loginEmail.Text;
-            string password = loginPassword.Text;
-
-            myDAL objmyDAl = new myDAL();
-
-            int status = 0;
-            int type = 0;
-            int id = 0;
-
-            status = objmyDAl.validateLogin(email, password, ref type, ref id);
-
-            if (status == 0)
-            {
-                Session["idoriginal"] = id;
-
-                if (type == 1)
-                {
-                    Response.BufferOutput = true;
-                    Response.Redirect("~/Patient/PatientHome.aspx");
-                    return;
-                }
-
-                else if (type == 2)
-                {
-                    Response.BufferOutput = true;
-                    Response.Redirect("~/Doctor/DoctorHome.aspx");
-                    return;
-                }
-
-                else if (type == 3)
-                {
-                    Response.BufferOutput = true;
-                    Response.Redirect("~/Admin/AdminHome.aspx");
-                    return;
-                }
-            }
-
-
-            else if (status == 1)
-            {
-                Response.Write("<script>alert('Email not found. Try Again !');</script>");
-            }
-
-            else if (status == 2)
-            {
-                Response.Write("<script>alert('Incorrect Password. Try Again !');</script>");
-            }
-
-            else if (status == -1)
-            {
-                Response.Write("<script>alert('There was some error. Try Again !');</script>");
-            }
-        }
-
 
 
 
